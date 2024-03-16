f77bfe7ab3c4dfe4af94af1283767dc039be70e1
Juan Pablo Duran <pablo_duran@unicomer.com>
2024-03-16 11:51:09 -0300

    esta la primera carga

---
+++ assets/js/login.js
@@ -0,0 +1,23 @@
+
+function verValorInput(){
+  const valorEscrito=  $("#inputUsuario").val();
+  alert(valorEscrito);
+  $("#inputUsuario").val(valorEscrito+" Agregado desde java");
+}
+
+   
+   
+   /*
+   const  labelUsuario = document.getElementById("labelUsuario");
+   const btnIngresa= document.getElementById("btnIngresar");
+
+   btnIngresa.addEventListener("click",()=>{
+    verValorInput()
+   });
+
+   function verValorInput(){
+    const  inputUsuario = document.getElementById("inputUsuario");
+        alert(inputUsuario.value)
+        alert(document.getElementById("inputUsuario").value)
+   }
+*/
